e1a2ed90bf8524c10521585661247dba44919c36
Thomas Gleixner <tglx@linutronix.de>
2011-07-17 21:56:42 +0200

    trace: Add migrate-disabled counter to tracing output
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/trace_events.h
+++ include/linux/trace_events.h
@@ -69,6 +69,7 @@ struct trace_entry {
 	unsigned char		flags;
 	unsigned char		preempt_count;
 	int			pid;
+	unsigned char		migrate_disable;
 };
 
 #define TRACE_EVENT_TYPE_MAX						\
@@ -157,6 +158,7 @@ static inline void tracing_generic_entry_update(struct trace_entry *entry,
 						unsigned int trace_ctx)
 {
 	entry->preempt_count		= trace_ctx & 0xff;
+	entry->migrate_disable		= (trace_ctx >> 8) & 0xff;
 	entry->pid			= current->pid;
 	entry->type			= type;
 	entry->flags =			trace_ctx >> 16;
