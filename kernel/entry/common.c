d4df20e59948d679875e18204f990d1694ccf507
Thomas Gleixner <tglx@linutronix.de>
2012-11-01 11:03:47 +0100

    x86: Support for lazy preemption
    Implement the x86 pieces for lazy preempt.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/entry/common.c
+++ kernel/entry/common.c
@@ -155,7 +155,7 @@ static unsigned long exit_to_user_mode_loop(struct pt_regs *regs,
 
 		local_irq_enable_exit_to_user(ti_work);
 
-		if (ti_work & _TIF_NEED_RESCHED)
+		if (ti_work & _TIF_NEED_RESCHED_MASK)
 			schedule();
 
 		if (ti_work & _TIF_UPROBE)
