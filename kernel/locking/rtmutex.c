f5a9d1008c4eecd571835dead40306537e20bfdd
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2015-12-02 11:34:07 +0100

    rtmutex: trylock is okay on -RT
    non-RT kernel could deadlock on rt_mutex_trylock() in softirq context. On
    -RT we don't run softirqs in IRQ context but in thread context so it is
    not a issue here.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -1962,7 +1962,11 @@ EXPORT_SYMBOL_GPL(rt_mutex_timed_lock);
 
 int __sched __rt_mutex_trylock(struct rt_mutex *lock)
 {
+#ifdef CONFIG_PREEMPT_RT
+	if (WARN_ON_ONCE(in_irq() || in_nmi()))
+#else
 	if (WARN_ON_ONCE(in_irq() || in_nmi() || in_serving_softirq()))
+#endif
 		return 0;
 
 	return rt_mutex_fasttrylock(lock, rt_mutex_slowtrylock);
