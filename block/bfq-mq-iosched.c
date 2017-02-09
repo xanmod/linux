6e412ac9dc583a0b2d7223c491d265ad4d402ee3
Paolo Valente <paolo.valente@linaro.org>
2017-02-09 10:36:27 +0100

    Add lock check in bfq_allow_bio_merge
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

--- block/bfq-mq-iosched.c
+++ block/bfq-mq-iosched.c
@@ -2279,6 +2279,7 @@ static bool bfq_allow_bio_merge(struct request_queue *q, struct request *rq,
 	if (!bic)
 		return false;
 
+	assert_spin_locked(&bfqd->lock);
 	bfqq = bic_to_bfqq(bic, is_sync);
 	/*
 	 * We take advantage of this function to perform an early merge
