72c180f13f4e7b31e4b95d9bf63c1c4e4f5916a2
Alexandre Frade <admfrade@gmail.com>
2017-05-01 16:33:14 +0000

    [PATCH] Initial XanMod 4.11 patches

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -143,7 +143,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 10;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
