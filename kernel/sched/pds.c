24e64531dd166e0e51a43dcb935a277a54f54d85
Alfred Chen <cchalpha@gmail.com>
2017-10-09 19:43:56 +0800

    pds: sync up get_user_cpu_mask().

--- kernel/sched/pds.c
+++ kernel/sched/pds.c
@@ -5054,16 +5054,15 @@ out_put_task:
 }
 
 static int get_user_cpu_mask(unsigned long __user *user_mask_ptr, unsigned len,
-			     cpumask_t *new_mask)
+			     struct cpumask *new_mask)
 {
-	if (len < sizeof(cpumask_t)) {
-		memset(new_mask, 0, sizeof(cpumask_t));
-	} else if (len > sizeof(cpumask_t)) {
-		len = sizeof(cpumask_t);
-	}
+	if (len < cpumask_size())
+		cpumask_clear(new_mask);
+	else if (len > cpumask_size())
+		len = cpumask_size();
+
 	return copy_from_user(new_mask, user_mask_ptr, len) ? -EFAULT : 0;
 }
-
 
 /**
  * sys_sched_setaffinity - set the CPU affinity of a process
