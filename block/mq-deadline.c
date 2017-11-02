56ee621c57747e2428ae6e7ce0ed0f22b94fa043
Alexandre Frade <admfrade@gmail.com>
2017-11-02 18:29:15 +0000

    mq-deadline: add 'deadline' as a name alias
    The scheduler framework now supports looking up the appropriate
    scheduler with the {name,mq} tupple. We can register mq-deadline
    with the alias of 'deadline', so that switching to 'deadline'
    will do the right thing based on the type of driver attached to
    it.
    
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Reviewed-by: Omar Sandoval <osandov@fb.com>

--- block/mq-deadline.c
+++ block/mq-deadline.c
@@ -658,6 +658,7 @@ static struct elevator_type mq_deadline = {
 #endif
 	.elevator_attrs = deadline_attrs,
 	.elevator_name = "mq-deadline",
+	.elevator_alias = "deadline",
 	.elevator_owner = THIS_MODULE,
 };
 
