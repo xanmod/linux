f69221ba05c9e1fc60af7e1ae9a73468ef3d1b28
Alexandre Frade <admfrade@gmail.com>
2018-01-29 17:36:22 +0000

    mm/zswap: set to use lz4 compressor
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -91,7 +91,7 @@ static struct kernel_param_ops zswap_enabled_param_ops = {
 module_param_cb(enabled, &zswap_enabled_param_ops, &zswap_enabled, 0644);
 
 /* Crypto compressor to use */
-#define ZSWAP_COMPRESSOR_DEFAULT "lzo"
+#define ZSWAP_COMPRESSOR_DEFAULT "lz4"
 static char *zswap_compressor = ZSWAP_COMPRESSOR_DEFAULT;
 static int zswap_compressor_param_set(const char *,
 				      const struct kernel_param *);
