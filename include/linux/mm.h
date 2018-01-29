75ca9c8cc192dbaa79fcc2a20a6f915c5d7e7617
Alexandre Frade <admfrade@gmail.com>
2018-01-29 17:21:43 +0000

    mm: set 128/2048 (min/max) kilobytes to read-ahead for filesystems on this block device
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2304,8 +2304,8 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	2048	/* kbytes */
+#define VM_MIN_READAHEAD	256	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
