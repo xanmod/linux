c01dbce0c811e52645130c286ce15975a5348220
Melzani Alessandro <melzani.alessandro@gmail.com>
2018-02-26 22:43:30 +0100

    bfq-sq, bfq-mq: port of "bfq: Use icq_to_bic() consistently"
    Some code uses icq_to_bic() to convert an io_cq pointer to a
    bfq_io_cq pointer while other code uses a direct cast. Convert
    the code that uses a direct cast such that it uses icq_to_bic().
    
    Signed-off-by: Alessandro Melzani <melzani.alessandro@gmail.com>

--- block/bfq-mq-iosched.c
+++ block/bfq-mq-iosched.c
@@ -272,7 +272,7 @@ static const unsigned long max_service_from_wr = 120000;
 #define BFQ_SERVICE_TREE_INIT	((struct bfq_service_tree)		\
 				{ RB_ROOT, RB_ROOT, NULL, NULL, 0, 0 })
 
-#define RQ_BIC(rq)		((struct bfq_io_cq *) (rq)->elv.priv[0])
+#define RQ_BIC(rq)		icq_to_bic((rq)->elv.priv[0])
 #define RQ_BFQQ(rq)		((rq)->elv.priv[1])
 
 /**
