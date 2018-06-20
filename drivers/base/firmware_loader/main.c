7879d55401932e0c56d5b549fa1bdb880bd73c6a
William Douglas <william.douglas@intel.com>
2018-06-20 17:23:21 +0000

    Enable stateless firmware loading
    Prefer the order of specific version before generic and /etc before
    /lib to enable the user to give specific overrides for generic
    firmware and distribution firmware.
    
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -278,6 +278,8 @@ static void free_fw_priv(struct fw_priv *fw_priv)
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+	"/etc/firmware/" UTS_RELEASE,
+	"/etc/firmware",
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
