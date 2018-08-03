3ab5fb9ad8e881b116d3e9656e12ab98b3dc0854
Alexandre Frade <admfrade@gmail.com>
2018-08-03 12:46:10 -0300

    block, scsi: Make SCSI quiesce and resume work reliably [v4.14.57+]

--- block/blk-core.c
+++ block/blk-core.c
@@ -820,6 +820,7 @@ int blk_queue_enter(struct request_queue *q, blk_mq_req_flags_t flags)
 	const bool preempt = flags & BLK_MQ_REQ_PREEMPT;
 
 	while (true) {
+		bool success = false;
 
 		rcu_read_lock_sched();
 		if (percpu_ref_tryget_live(&q->q_usage_counter)) {
@@ -852,7 +853,8 @@ int blk_queue_enter(struct request_queue *q, blk_mq_req_flags_t flags)
 		smp_rmb();
 
 		wait_event(q->mq_freeze_wq,
-			   !atomic_read(&q->mq_freeze_depth) ||
+			   (atomic_read(&q->mq_freeze_depth) == 0 &&
+			   (preempt || !blk_queue_preempt_only(q))) ||
 			   blk_queue_dying(q));
 		if (blk_queue_dying(q))
 			return -ENODEV;
