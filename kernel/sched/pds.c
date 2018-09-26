79d6f718b7e73a5fecdd78f60920b1ed363d6618
Alfred Chen <cchalpha@gmail.com>
2018-09-26 13:58:30 +0800

    Tag PDS 0.99a

--- kernel/sched/pds.c
+++ kernel/sched/pds.c
@@ -88,7 +88,7 @@ enum {
 
 static inline void print_scheduler_version(void)
 {
-	printk(KERN_INFO "pds: PDS-mq CPU Scheduler 0.98z by Alfred Chen.\n");
+	printk(KERN_INFO "pds: PDS-mq CPU Scheduler 0.99a by Alfred Chen.\n");
 }
 
 /* task_struct::on_rq states: */
