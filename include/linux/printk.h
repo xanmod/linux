fa644ef8b55461d21af563c6d6a2aa5987642036
John Ogness <john.ogness@linutronix.de>
2019-02-12 15:30:00 +0100

    printk: implement /dev/kmsg
    Since printk messages are now logged to a new ring buffer, update
    the /dev/kmsg functions to pull the messages from there.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -192,6 +192,7 @@ __printf(1, 2) void dump_stack_set_arch_desc(const char *fmt, ...);
 void dump_stack_print_info(const char *log_lvl);
 void show_regs_print_info(const char *log_lvl);
 extern asmlinkage void dump_stack(void) __cold;
+struct wait_queue_head *printk_wait_queue(void);
 #else
 static inline __printf(1, 0)
 int vprintk(const char *s, va_list args)
