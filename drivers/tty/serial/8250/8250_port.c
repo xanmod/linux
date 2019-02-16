13e24b362602f8d6867a4a3a893979868460a3ca
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-02-16 09:02:00 +0100

    serial: 8250: export symbols which are used by symbols
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -2117,6 +2117,7 @@ void clear_ier(struct uart_8250_port *up)
 	}
 	console_atomic_unlock(flags);
 }
+EXPORT_SYMBOL_GPL(clear_ier);
 
 void restore_ier(struct uart_8250_port *up)
 {
@@ -2128,6 +2129,7 @@ void restore_ier(struct uart_8250_port *up)
 		serial_port_out(port, UART_IER, atomic_read(&ier_value));
 	console_atomic_unlock(flags);
 }
+EXPORT_SYMBOL_GPL(restore_ier);
 
 #ifdef CONFIG_CONSOLE_POLL
 /*
