e900f7b23d55f1e090b969b97a9c71e049b05352
Alfred Chen <cchalpha@gmail.com>
2019-04-10 14:08:41 +0800

    Tag BMQ v0.92

--- kernel/sched/bmq.c
+++ kernel/sched/bmq.c
@@ -56,7 +56,7 @@
 
 static inline void print_scheduler_version(void)
 {
-	printk(KERN_INFO "bmq: BMQ CPU Scheduler 0.91 by Alfred Chen.\n");
+	printk(KERN_INFO "bmq: BMQ CPU Scheduler 0.92 by Alfred Chen.\n");
 }
 
 /**
