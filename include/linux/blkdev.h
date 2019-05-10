9531167c7cb2a8379966b48eb6df60b1950154d6
Alexandre Frade <admfrade@gmail.com>
2019-05-10 16:45:59 -0300

    block: set rq_affinity = 2 for full multithreading I/O requests
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -619,7 +619,8 @@ struct request_queue {
 #define QUEUE_FLAG_RQ_ALLOC_TIME 27	/* record rq->alloc_time_ns */
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
-				 (1 << QUEUE_FLAG_SAME_COMP))
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE))
 
 void blk_queue_flag_set(unsigned int flag, struct request_queue *q);
 void blk_queue_flag_clear(unsigned int flag, struct request_queue *q);
