ba8fae94491e5067096b67dda66dcbadc92d97a9
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-07-26 11:30:49 +0200

    Use CONFIG_PREEMPTION
    Thisi is an all-in-one patch of the current `PREEMPTION' branch.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -3654,6 +3654,8 @@ print_trace_header(struct seq_file *m, struct trace_iterator *iter)
 		   "desktop",
 #elif defined(CONFIG_PREEMPT)
 		   "preempt",
+#elif defined(CONFIG_PREEMPT_RT)
+		   "preempt_rt",
 #else
 		   "unknown",
 #endif
