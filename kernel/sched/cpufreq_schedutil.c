3c4e6ea533c6c9f7f99cfe3dff3b7327f72265eb
Alexandre Frade <admfrade@gmail.com>
2019-08-25 19:03:59 -0300

    bmq: merge fix for kernel v5.2.10 [1/2]
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -434,9 +434,7 @@ static inline bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu) { return false; }
  */
 static inline void ignore_dl_rate_limit(struct sugov_cpu *sg_cpu, struct sugov_policy *sg_policy)
 {
-#ifndef CONFIG_SCHED_BMQ
 	if (cpu_bw_dl(cpu_rq(sg_cpu->cpu)) > sg_cpu->bw_dl)
-#endif
 		sg_policy->need_freq_update = true;
 }
 
