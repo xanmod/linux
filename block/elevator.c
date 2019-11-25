3785e9822c8bb21b926c3fdf1054d5a7d29904b0
Alexandre Frade <kernel@xanmod.org>
2019-11-25 15:13:06 -0300

    elevator: set default scheduler to bfq for blk-mq
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- block/elevator.c
+++ block/elevator.c
@@ -616,15 +616,15 @@ static inline bool elv_support_iosched(struct request_queue *q)
 }
 
 /*
- * For single queue devices, default to using mq-deadline. If we have multiple
- * queues or mq-deadline is not available, default to "none".
+ * For single queue devices, default to using bfq. If we have multiple
+ * queues or bfq is not available, default to "none".
  */
 static struct elevator_type *elevator_get_default(struct request_queue *q)
 {
 	if (q->nr_hw_queues != 1)
 		return NULL;
 
-	return elevator_get(q, "mq-deadline", false);
+	return elevator_get(q, "bfq", false);
 }
 
 /*
