c42fd14c1d770a0a4e7bb80eab2ab681cd7fbae8
Alexandre Frade <admfrade@gmail.com>
2019-12-25 23:06:31 -0300

    Revert "sched/core: nr_migrate = 128 increases number of tasks to iterate in a single balance run."
    This reverts commit edb07e3fce7676c87eb4838a8b98e26a4aaca225.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -56,7 +56,7 @@ const_debug unsigned int sysctl_sched_features =
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
-const_debug unsigned int sysctl_sched_nr_migrate = 128;
+const_debug unsigned int sysctl_sched_nr_migrate = 32;
 
 /*
  * period over which we measure -rt task CPU usage in us.
@@ -68,9 +68,9 @@ __read_mostly int scheduler_running;
 
 /*
  * part of the period that we allow rt tasks to run in us.
- * XanMod default: 0.98s
+ * default: 0.95s
  */
-int sysctl_sched_rt_runtime = 980000;
+int sysctl_sched_rt_runtime = 950000;
 
 /*
  * __task_rq_lock - lock the rq @p resides on.
