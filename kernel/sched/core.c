1997af8fe8f5452f2a5287f9ef827bfc843703fa
Alexandre Frade <admfrade@gmail.com>
2020-01-05 10:06:42 -0300

    sched/core: Increase nr_migrate to 256 tasks to iterate in a single balance run
    Signed-off-by: Alexandre Frade <admfrade@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -44,7 +44,7 @@ const_debug unsigned int sysctl_sched_features =
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
-const_debug unsigned int sysctl_sched_nr_migrate = 128;
+const_debug unsigned int sysctl_sched_nr_migrate = 256;
 
 /*
  * period over which we measure -rt task CPU usage in us.
