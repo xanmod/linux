7365bbf5b570044ffbb66bc31af6cd5442b96cea
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2020-03-30 16:10:48 +0200

    mm/compaction: Disable compact_unevictable_allowed on RT (Update)
    This is an all-on-one change to align the state of the patch with what
    has been submit upstream. This includes the following patches:
    
      [PATCH 1/2] mm/compaction: Really limit compact_unevictable_allowed to 0…1
      [PATCH 2/2 v5] mm/compaction: Disable compact_unevictable_allowed on RT
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1591,7 +1591,7 @@ typedef enum {
  * compactable pages.
  */
 #ifdef CONFIG_PREEMPT_RT
-#define sysctl_compact_unevictable_allowed 0
+int sysctl_compact_unevictable_allowed __read_mostly = 0;
 #else
 int sysctl_compact_unevictable_allowed __read_mostly = 1;
 #endif
