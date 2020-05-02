1e465f94bfe31df6656c4d1190b10787e570a27c
Alexandre Frade <admfrade@gmail.com>
2020-05-02 16:36:29 -0300

    pci: Fix merge pcie_acs_overrides attr to pci_dev_acs_enabled function [2/2]

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -4896,6 +4896,8 @@ static const struct pci_dev_acs_enabled {
 	{ PCI_VENDOR_ID_ZHAOXIN, 0x9083, pci_quirk_mf_endpoint_acs },
 	/* Zhaoxin Root/Downstream Ports */
 	{ PCI_VENDOR_ID_ZHAOXIN, PCI_ANY_ID, pci_quirk_zhaoxin_pcie_ports_acs },
+	/* PCIe ACS overrides */
+	{ PCI_ANY_ID, PCI_ANY_ID, pcie_acs_overrides },
 	{ 0 }
 };
 
