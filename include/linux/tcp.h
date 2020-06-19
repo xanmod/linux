7c7d8006b6b944281caaba3ae2f8127f7ed0668b
Jianfeng Wang <jfwang@google.com>
2020-06-19 17:33:45 +0000

    net-tcp_bbr: v2: record app-limited status of TLP-repaired flight
    When sending a TLP retransmit, record whether the outstanding flight
    of data is application limited. This is important for congestion
    control modules that want to respond to losses repaired by TLP
    retransmits. This is important because the following scenarios convey
    very different information:
     (1) a packet loss with a small number of packets in flight;
     (2) a packet loss with the maximum amount of data in flight allowed
         by the CC module;
    
    Effort: net-tcp_bbr
    Change-Id: Ic8ae567caa4e4bfd5fd82c3d4be12a5d9171655e
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -274,7 +274,8 @@ struct tcp_sock {
 		tlp_retrans:1,	/* TLP is a retransmission */
 		tcp_usec_ts:1, /* TSval values in usec */
 		fast_ack_mode:2, /* which fast ack mode ? */
-		unused:2;
+		tlp_orig_data_app_limited:1, /* app-limited before TLP rtx? */
+		unused:1;
 	u32	chrono_start;	/* Start time in jiffies of a TCP chrono */
 	u32	chrono_stat[3];	/* Time in jiffies for chrono_stat stats */
 	u8	chrono_type:2,	/* current chronograph type */
