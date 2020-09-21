283c0632fb24c1031e81acaece5d059f0047c4eb
Neal Cardwell <ncardwell@google.com>
2020-09-21 14:46:26 -0400

    net-tcp_bbr: v2: introduce is_acking_tlp_retrans_seq into rate_sample
    Introduce is_acking_tlp_retrans_seq into rate_sample. This bool will
    export to the CC module the knowledge of whether the current ACK
    matched a TLP retransmit.
    
    Note that when this bool is true, we cannot yet tell (in general) whether
    this ACK is for the original or the TLP retransmit.
    
    Effort: net-tcp_bbr
    Change-Id: I2e6494332167e75efcbdc99bd5c119034e9c39b4
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -3815,7 +3815,8 @@ static void tcp_replace_ts_recent(struct tcp_sock *tp, u32 seq)
 /* This routine deals with acks during a TLP episode and ends an episode by
  * resetting tlp_high_seq. Ref: TLP algorithm in draft-ietf-tcpm-rack
  */
-static void tcp_process_tlp_ack(struct sock *sk, u32 ack, int flag)
+static void tcp_process_tlp_ack(struct sock *sk, u32 ack, int flag,
+				struct rate_sample *rs)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 
@@ -3843,6 +3844,11 @@ static void tcp_process_tlp_ack(struct sock *sk, u32 ack, int flag)
 			     FLAG_NOT_DUP | FLAG_DATA_SACKED))) {
 		/* Pure dupack: original and TLP probe arrived; no loss */
 		tp->tlp_high_seq = 0;
+	} else {
+		/* This ACK matches a TLP retransmit. We cannot yet tell if
+		 * this ACK is for the original or the TLP retransmit.
+		 */
+		rs->is_acking_tlp_retrans_seq = 1;
 	}
 }
 
@@ -4026,7 +4032,7 @@ static int tcp_ack(struct sock *sk, const struct sk_buff *skb, int flag)
 	tcp_rack_update_reo_wnd(sk, &rs);
 
 	if (tp->tlp_high_seq)
-		tcp_process_tlp_ack(sk, ack, flag);
+		tcp_process_tlp_ack(sk, ack, flag, &rs);
 
 	if (tcp_ack_is_dubious(sk, flag)) {
 		if (!(flag & (FLAG_SND_UNA_ADVANCED |
@@ -4070,7 +4076,7 @@ no_queue:
 	tcp_ack_probe(sk);
 
 	if (tp->tlp_high_seq)
-		tcp_process_tlp_ack(sk, ack, flag);
+		tcp_process_tlp_ack(sk, ack, flag, &rs);
 	return 1;
 
 old_ack:
