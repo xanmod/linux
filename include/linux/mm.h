83e82a6bd76367f8b80e64fdaffff45443d3268a
Alexandre Frade <kernel@xanmod.org>
2020-10-29 14:11:56 +0000

    mm: set to 8 megabytes for address_space-level file read-ahead pages size
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2450,7 +2450,7 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_READAHEAD_PAGES	(SZ_2M / PAGE_SIZE)
+#define VM_READAHEAD_PAGES	(SZ_8M / PAGE_SIZE)
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
