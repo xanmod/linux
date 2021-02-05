9970c3df4edb7199bbf6b2f30f7fa8d5e13d7657
André Almeida <andrealmeid@collabora.com>
2021-02-05 10:34:00 -0300

    futex2: Implement vectorized wait
    Add support to wait on multiple futexes. This is the interface
    implemented by this syscall:
    
    futex_waitv(struct futex_waitv *waiters, unsigned int nr_futexes,
    	    unsigned int flags, struct timespec *timo)
    
    struct futex_waitv {
    	void *uaddr;
    	unsigned int val;
    	unsigned int flags;
    };
    
    Given an array of struct futex_waitv, wait on each uaddr. The thread
    wakes if a futex_wake() is performed at any uaddr. The syscall returns
    immediately if any waiter has *uaddr != val. *timo is an optional
    timeout value for the operation. The flags argument of the syscall
    should be used solely for specifying the timeout as realtime, if needed.
    Flags for shared futexes, sizes, etc. should be used on the individual
    flags of each waiter.
    
    Returns the array index of one of the awakened futexes. Thereâ€™s no given
    information of how many were awakened, or any particular attribute of it
    (if itâ€™s the first awakened, if it is of the smaller index...).
    
    Signed-off-by: AndrÃ© Almeida <andrealmeid@collabora.com>
    
    Rebased-by: Joshua Ashton <joshua@froggi.es>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -884,8 +884,11 @@ __SYSCALL(__NR_futex_wait, sys_futex_wait)
 #define __NR_futex_wake 449
 __SYSCALL(__NR_futex_wake, sys_futex_wake)
 
+#define __NR_futex_waitv 450
+__SC_COMP(__NR_futex_waitv, sys_futex_waitv, compat_sys_futex_waitv)
+
 #undef __NR_syscalls
-#define __NR_syscalls 450
+#define __NR_syscalls 451
 
 /*
  * 32 bit systems traditionally used different
