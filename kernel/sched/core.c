19de4ac85f4b81461086eb05781e6526e52b8380
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-09 05:21:50 +0300

    add -r2 in CacULE dmesg

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8107,7 +8107,7 @@ void __init sched_init(void)
 #endif
 
 #ifdef CONFIG_CACULE_SCHED
-	printk(KERN_INFO "CacULE CPU scheduler v5.12 by Hamad Al Marri.");
+	printk(KERN_INFO "CacULE CPU scheduler v5.12-r2 by Hamad Al Marri.");
 #endif
 
 	wait_bit_init();
