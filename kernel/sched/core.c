d91d2bbe9700ee30e3373c796fbd7c45f74e0aba
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-09 05:19:59 +0300

    cleanup

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3554,11 +3554,10 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.sum_exec_runtime		= 0;
 	p->se.prev_sum_exec_runtime	= 0;
 	p->se.nr_migrations		= 0;
+	p->se.vruntime			= 0;
 
 #ifdef CONFIG_CACULE_SCHED
 	p->se.cacule_node.vruntime	= 0;
-#else
-	p->se.vruntime			= 0;
 #endif
 
 	INIT_LIST_HEAD(&p->se.group_node);
