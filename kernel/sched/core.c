76d7ea767006e1caa29de160f4e2fb9a81bff990
Hamad Al Marri <hamad@cachyos.org>
2021-06-30 18:03:00 +0300

    remove delta since it is not used.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4680,7 +4680,9 @@ static void sched_tick_remote(struct work_struct *work)
 	struct rq *rq = cpu_rq(cpu);
 	struct task_struct *curr;
 	struct rq_flags rf;
+#if !defined(CONFIG_CACULE_SCHED)
 	u64 delta;
+#endif
 	int os;
 
 	/*
