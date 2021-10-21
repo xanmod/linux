bd018ff94b328779943f66a1014de403726498e9
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-20 20:50:32 -0700

    ZEN: mm: Don't hog the CPU and zone lock in rmqueue_bulk()
    There is noticeable scheduling latency and heavy zone lock contention
    stemming from rmqueue_bulk's single hold of the zone lock while doing
    its work, as seen with the preemptoff tracer. There's no actual need for
    rmqueue_bulk() to hold the zone lock the entire time; it only does so
    for supposed efficiency. As such, we can relax the zone lock and even
    reschedule when IRQs are enabled in order to keep the scheduling delays
    and zone lock contention at bay. Forward progress is still guaranteed,
    as the zone lock can only be relaxed after page removal.
    
    With this change, rmqueue_bulk() no longer appears as a serious offender
    in the preemptoff tracer, and system latency is noticeably improved.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2117,16 +2117,17 @@ retry:
 }
 
 /*
- * Obtain a specified number of elements from the buddy allocator, all under
- * a single hold of the lock, for efficiency.  Add them to the supplied list.
- * Returns the number of new pages which were placed at *list.
+ * Obtain a specified number of elements from the buddy allocator, and relax the
+ * zone lock when needed. Add them to the supplied list. Returns the number of
+ * new pages which were placed at *list.
  */
 static int rmqueue_bulk(struct zone *zone, unsigned int order,
 			unsigned long count, struct list_head *list,
 			int migratetype, unsigned int alloc_flags)
 {
+	const bool can_resched = !preempt_count() && !irqs_disabled();
 	unsigned long flags;
-	int i;
+	int i, last_mod = 0;
 
 	spin_lock_irqsave(&zone->lock, flags);
 	for (i = 0; i < count; ++i) {
@@ -2134,6 +2135,18 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 								alloc_flags);
 		if (unlikely(page == NULL))
 			break;
+
+		/* Reschedule and ease the contention on the lock if needed */
+		if (i + 1 < count && ((can_resched && need_resched()) ||
+				      spin_needbreak(&zone->lock))) {
+			__mod_zone_page_state(zone, NR_FREE_PAGES,
+					      -((i + 1 - last_mod) << order));
+			last_mod = i + 1;
+			spin_unlock_irqrestore(&zone->lock, flags);
+			if (can_resched)
+				cond_resched();
+			spin_lock_irqsave(&zone->lock, flags);
+		}
 
 		/*
 		 * Split buddy pages returned by expand() are received here in
@@ -2151,7 +2164,7 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 					      -(1 << order));
 	}
 
-	__mod_zone_page_state(zone, NR_FREE_PAGES, -(i << order));
+	__mod_zone_page_state(zone, NR_FREE_PAGES, -((i - last_mod) << order));
 	spin_unlock_irqrestore(&zone->lock, flags);
 
 	return i;
