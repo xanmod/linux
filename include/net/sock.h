98bd7acf59ed8b59e9dd6d88d9903e288026ec9d
Eric Dumazet <edumazet@google.com>
2021-11-15 11:02:33 -0800

    net: use sk_is_tcp() in more places
    Move sk_is_tcp() to include/net/sock.h and use it where we can.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2624,6 +2624,11 @@ static inline void skb_setup_tx_timestamp(struct sk_buff *skb, __u16 tsflags)
 			   &skb_shinfo(skb)->tskey);
 }
 
+static inline bool sk_is_tcp(const struct sock *sk)
+{
+	return sk->sk_type == SOCK_STREAM && sk->sk_protocol == IPPROTO_TCP;
+}
+
 DECLARE_STATIC_KEY_FALSE(tcp_rx_skb_cache_key);
 /**
  * sk_eat_skb - Release a skb if it is no longer needed
