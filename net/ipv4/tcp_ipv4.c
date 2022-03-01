ebe0743107855aa1fd883731e7412cfe55a7a391
mfreemon@cloudflare.com <mfreemon@cloudflare.com>
2022-03-01 17:06:02 -0600

    tcp: Add a sysctl to skip tcp collapse processing when the receive buffer is full
    For context and additional information about this patch, see the
    blog post at https://blog.cloudflare.com/optimizing-tcp-for-high-throughput-and-low-latency/
    
    sysctl:  net.ipv4.tcp_collapse_max_bytes
    
    If tcp_collapse_max_bytes is non-zero, attempt to collapse the
    queue to free up memory if the current amount of memory allocated
    is less than tcp_collapse_max_bytes.  Otherwise, the packet is
    dropped without attempting to collapse the queue.
    
    If tcp_collapse_max_bytes is zero, this feature is disabled
    and the default Linux behavior is used.  The default Linux
    behavior is to always perform the attempt to collapse the
    queue to free up memory.
    
    When the receive queue is small, we want to collapse the
    queue.  There are two reasons for this: (a) the latency of
    performing the collapse will be small on a small queue, and
    (b) we want to avoid sending a congestion signal (via a
    packet drop) to the sender when the receive queue is small.
    
    The result is that we avoid latency spikes caused by the
    time it takes to perform the collapse logic when the receive
    queue is large and full, while preserving existing behavior
    and performance for all other cases.
    
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -3497,6 +3497,7 @@ static int __net_init tcp_sk_init(struct net *net)
 
 	net->ipv4.sysctl_tcp_syn_linear_timeouts = 4;
 	net->ipv4.sysctl_tcp_shrink_window = 0;
+	net->ipv4.sysctl_tcp_collapse_max_bytes = 0;
 
 	net->ipv4.sysctl_tcp_pingpong_thresh = 1;
 
