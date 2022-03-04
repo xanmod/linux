aab3593b41f3deabef3b0c83b898109fdbe018e1
Alexandre Frade <kernel@xanmod.org>
2022-03-04 20:53:57 +0000

    Revert "net: use sk_is_tcp() in more places"
    This reverts commit 98bd7acf59ed8b59e9dd6d88d9903e288026ec9d.

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -4842,7 +4842,8 @@ static void __skb_complete_tx_timestamp(struct sk_buff *skb,
 	serr->header.h4.iif = skb->dev ? skb->dev->ifindex : 0;
 	if (sk->sk_tsflags & SOF_TIMESTAMPING_OPT_ID) {
 		serr->ee.ee_data = skb_shinfo(skb)->tskey;
-		if (sk_is_tcp(sk))
+		if (sk->sk_protocol == IPPROTO_TCP &&
+		    sk->sk_type == SOCK_STREAM)
 			serr->ee.ee_data -= sk->sk_tskey;
 	}
 
@@ -4911,7 +4912,8 @@ void __skb_tstamp_tx(struct sk_buff *orig_skb,
 	if (tsonly) {
 #ifdef CONFIG_INET
 		if ((sk->sk_tsflags & SOF_TIMESTAMPING_OPT_STATS) &&
-		    sk_is_tcp(sk)) {
+		    sk->sk_protocol == IPPROTO_TCP &&
+		    sk->sk_type == SOCK_STREAM) {
 			skb = tcp_get_timestamping_opt_stats(sk, orig_skb,
 							     ack_skb);
 			opt_stats = true;
