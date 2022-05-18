93f31833fac44e79afb61523cbac4ed56a30d352
Yu Zhao <yuzhao@google.com>
2022-05-17 19:46:29 -0600

    mm: multi-gen LRU: kill switch
    Add /sys/kernel/mm/lru_gen/enabled as a kill switch. Components that
    can be disabled include:
      0x0001: the multi-gen LRU core
      0x0002: walking page table, when arch_has_hw_pte_young() returns
              true
      0x0004: clearing the accessed bit in non-leaf PMD entries, when
              CONFIG_ARCH_HAS_NONLEAF_PMD_YOUNG=y
      [yYnN]: apply to all the components above
    E.g.,
      echo y >/sys/kernel/mm/lru_gen/enabled
      cat /sys/kernel/mm/lru_gen/enabled
      0x0007
      echo 5 >/sys/kernel/mm/lru_gen/enabled
      cat /sys/kernel/mm/lru_gen/enabled
      0x0005
    
    NB: the page table walks happen on the scale of seconds under heavy
    memory pressure, in which case the mmap_lock contention is a lesser
    concern, compared with the LRU lock contention and the I/O congestion.
    So far the only well-known case of the mmap_lock contention happens on
    Android, due to Scudo [1] which allocates several thousand VMAs for
    merely a few hundred MBs. The SPF and the Maple Tree also have
    provided their own assessments [2][3]. However, if walking page tables
    does worsen the mmap_lock contention, the kill switch can be used to
    disable it. In this case the multi-gen LRU will suffer a minor
    performance degradation, as shown previously.
    
    Clearing the accessed bit in non-leaf PMD entries can also be
    disabled, since this behavior was not tested on x86 varieties other
    than Intel and AMD.
    
    [1] https://source.android.com/devices/tech/debug/scudo
    [2] https://lore.kernel.org/r/20220128131006.67712-1-michel@lespinasse.org/
    [3] https://lore.kernel.org/r/20220426150616.3937571-1-Liam.Howlett@oracle.com/
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Acked-by: Brian Geffon <bgeffon@google.com>
    Acked-by: Jan Alexander Steffens (heftig) <heftig@archlinux.org>
    Acked-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Acked-by: Steven Barrett <steven@liquorix.net>
    Acked-by: Suleiman Souhlal <suleiman@google.com>
    Tested-by: Daniel Byrne <djbyrne@mtu.edu>
    Tested-by: Donald Carr <d@chaos-reins.com>
    Tested-by: Holger Hoffstätte <holger@applied-asynchrony.com>
    Tested-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    Tested-by: Shuang Zhai <szhai2@cs.rochester.edu>
    Tested-by: Sofia Trinh <sofia.trinh@edi.works>
    Tested-by: Vaibhav Jain <vaibhav@linux.ibm.com>

--- include/linux/mm_inline.h
+++ include/linux/mm_inline.h
@@ -106,7 +106,15 @@ static __always_inline enum lru_list folio_lru_list(struct folio *folio)
 
 static inline bool lru_gen_enabled(void)
 {
-	return true;
+#ifdef CONFIG_LRU_GEN_ENABLED
+	DECLARE_STATIC_KEY_TRUE(lru_gen_caps[NR_LRU_GEN_CAPS]);
+
+	return static_branch_likely(&lru_gen_caps[LRU_GEN_CORE]);
+#else
+	DECLARE_STATIC_KEY_FALSE(lru_gen_caps[NR_LRU_GEN_CAPS]);
+
+	return static_branch_unlikely(&lru_gen_caps[LRU_GEN_CORE]);
+#endif
 }
 
 static inline bool lru_gen_in_fault(void)
@@ -219,7 +227,7 @@ static inline bool lru_gen_add_folio(struct lruvec *lruvec, struct folio *folio,
 
 	VM_WARN_ON_ONCE_FOLIO(gen != -1, folio);
 
-	if (folio_test_unevictable(folio))
+	if (folio_test_unevictable(folio) || !lrugen->enabled)
 		return false;
 	/*
 	 * There are three common cases for this page:
