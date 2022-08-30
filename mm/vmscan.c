d026d1eec80fecdff1870292579436bfdce924d1
Alexandre Frade <kernel@xanmod.org>
2022-08-30 01:33:28 +0000

    Revert "XANMOD: mm: multi-gen LRU: Add Hz min_ttl_ms for thrashing prevention"
    This reverts commit 092837029b12b8f368f94b723850e2b385593f99.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4434,7 +4434,7 @@ static bool age_lruvec(struct lruvec *lruvec, struct scan_control *sc, unsigned 
 }
 
 /* to protect the working set of the last N jiffies */
-static unsigned long lru_gen_min_ttl __read_mostly = HZ;
+static unsigned long lru_gen_min_ttl __read_mostly;
 
 static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 {
