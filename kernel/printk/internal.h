3794e4e65ee24154f5d56c9b1a74a41451cbd855
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2023-11-10 16:26:47 +0100

    printk: Make nbcon_get_default_prio() static inline
    Make nbcon_get_default_prio() static inline in the !PRINTK case.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/printk/internal.h
+++ kernel/printk/internal.h
@@ -178,7 +178,7 @@ static inline void nbcon_seq_force(struct console *con, u64 seq) { }
 static inline bool nbcon_alloc(struct console *con) { return false; }
 static inline void nbcon_init(struct console *con) { }
 static inline void nbcon_free(struct console *con) { }
-enum nbcon_prio nbcon_get_default_prio(void) { return NBCON_PRIO_NONE; }
+static inline enum nbcon_prio nbcon_get_default_prio(void) { return NBCON_PRIO_NONE; }
 static inline void nbcon_atomic_flush_all(void) { }
 static bool nbcon_atomic_emit_next_record(struct console *con) { return false; }
 
