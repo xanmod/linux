f1b2b0db528f7ab2cefecfa105cf4923835456a1
Alexandre Frade <kernel@xanmod.org>
2024-01-10 16:46:25 -0300

    Fix merge [1/2]: "tcp: introduce per-route feature RTAX_FEATURE_ECN_LOW"
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -725,15 +725,6 @@ static inline void tcp_fast_path_check(struct sock *sk)
 		tcp_fast_path_on(tp);
 }
 
-static inline void tcp_set_ecn_low_from_dst(struct sock *sk,
-					    const struct dst_entry *dst)
-{
-	struct tcp_sock *tp = tcp_sk(sk);
-
-	if (dst_feature(dst, RTAX_FEATURE_ECN_LOW))
-		tp->ecn_flags |= TCP_ECN_LOW;
-}
-
 /* Compute the actual rto_min value */
 static inline u32 tcp_rto_min(struct sock *sk)
 {
