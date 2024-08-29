42e3be62b3bc508091c3ba5431fa448a6c86a35a
Alexandre Frade <kernel@xanmod.org>
2024-08-29 22:39:20 +0000

    Revert "rcutorture: Also force sched priority to timersd on boosting test."
    This reverts commit 0b3cadf084c290f91428e7c0feb607b5ab9961cd.

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -637,7 +637,7 @@ static inline void tick_irq_exit(void)
 }
 
 #ifdef CONFIG_PREEMPT_RT
-DEFINE_PER_CPU(struct task_struct *, timersd);
+static DEFINE_PER_CPU(struct task_struct *, timersd);
 static DEFINE_PER_CPU(unsigned long, pending_timer_softirq);
 
 static unsigned int local_pending_timers(void)
